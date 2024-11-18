f9dd9220063ac7ff1721a56800e6e38d2b1ffa03
Roman <roman@macbook.local>
2024-11-18 18:28:48 +0200

    SIP

---
+++ phone/scripts/config-sample.js
@@ -0,0 +1,25 @@
+// var user = {
+//     //  User Name
+//     "User" : "sipuser",
+//     //  Password
+//     "Pass" : "yourpassword",
+//     //  Auth Realm
+//     "Realm"   : "sip.sample.com",
+//     // Display Name
+//     "Display" : "Gob Bleuth",
+//     // WebSocket URL
+//     "WSServer"  : "wss://wss.sample.com:8443"
+// };
+
+// var user = {
+//     //  User Name
+//     "User" : "1060",
+//     //  Password
+//     "Pass" : "1060",
+//     //  Auth Realm
+//     "Realm"   : "192.168.8.88",
+//     // Display Name
+//     "Display" : "1060",
+//     // WebSocket URL
+//     "WSServer"  : "wss://192.168.8.88:5063"
+// };
